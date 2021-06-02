84a751a4c54bd08473f8975003ccc57b3b973ca9
chenlang <lang.chen@unspay.com>
2021-06-02 19:53:22 +0800

    增加一个仿WeChat视频播放的自定义用户控件

---
+++ RRQMSkin/Converter/BoolConvertVisibility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RRQMSkin.Converter
+{
+    class BoolConvertVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //gender
+            if (value != null)
+            {
+                if ((bool)value == true)
+                    return Visibility.Collapsed;
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
