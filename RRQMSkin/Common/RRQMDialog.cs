ba94f6a0ed2e9633ebfe9c0abd128543f9f849fb
若汝棋茗 <76547834+RRQM@users.noreply.github.com>
2021-06-19 21:58:23 +0800

    增加提示框

---
+++ RRQMSkin/Common/RRQMDialog.cs
@@ -0,0 +1,53 @@
+﻿using RRQMSkin.Windows;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+
+namespace RRQMSkin
+{
+    public class RRQMDialog
+    {
+        public RRQMDialog(Window window = null)
+        {
+            this.window = window;
+            Timer timer = new Timer(10);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+        Window window;
+        int tick;
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (tick == 0 || tick > 200)
+            {
+                tick = 0;
+                if (queue.TryDequeue(out string msg))
+                {
+                    tick = 1;
+                    Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            DialogWindow dialogWindow = new DialogWindow(msg);
+                            dialogWindow.Owner = window;
+                            dialogWindow.Show();
+                        });
+                }
+            }
+            else
+            {
+                tick++;
+            }
+
+        }
+
+        ConcurrentQueue<string> queue = new ConcurrentQueue<string>();
+        public void Show(string mes)
+        {
+            queue.Enqueue(mes);
+        }
+    }
+}
---
+++ RRQMSkin/Windows/DialogWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RRQMSkin.Windows
+{
+    /// <summary>
+    /// DialogWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class DialogWindow : Window
+    {
+        public DialogWindow(string mes)
+        {
+            InitializeComponent();
+            this.Tb_Content.Text = mes;
+            this.Loaded += this.DialogWindow_Loaded;
+        }
+
+        private void DialogWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation_Top = new DoubleAnimation();
+            animation_Top.To = this.Owner.Top;
+            animation_Top.BeginTime = TimeSpan.FromSeconds(3);
+            animation_Top.Duration = TimeSpan.FromSeconds(1);
+            animation_Top.Completed += this.Animation_Top_Completed;
+            this.BeginAnimation(Window.TopProperty, animation_Top);
+
+            DoubleAnimation animation_Op = new DoubleAnimation();
+            animation_Op.To = 0;
+            animation_Op.BeginTime = TimeSpan.FromSeconds(3);
+            animation_Op.Duration = TimeSpan.FromSeconds(1);
+            this.BeginAnimation(Window.OpacityProperty, animation_Op);
+        }
+
+        private void Animation_Top_Completed(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
