a101e30db87bd449e1cf5dcb36c1f2e47c60bf0d
若汝棋茗 <505554090@qq.com>
2021-08-30 09:16:24 +0800

    T

---
+++ RRQMSkin/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinitionAttribute("RRQM_MVVM", "RRQMSkin.MVVM")]
+[assembly: XmlnsDefinitionAttribute("RRQM_Windows", "RRQMSkin.Windows")]
+[assembly: XmlnsDefinitionAttribute("RRQM_Controls", "RRQMSkin.Controls")]
+[assembly: XmlnsDefinitionAttribute("RRQM_DragDrop", "RRQMSkin.DragDrop")]
+[assembly: XmlnsDefinitionAttribute("RRQM_Charts", "RRQMSkin.Charts")]
+[assembly: XmlnsDefinitionAttribute("RRQM_Primitives", "RRQMSkin.Primitives")]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
