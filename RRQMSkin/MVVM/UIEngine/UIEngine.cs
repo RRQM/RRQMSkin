6d8774779b91f8697c2ec65d0a4a3d3894f4485b
若汝棋茗 <76547834+RRQM@users.noreply.github.com>
2022-01-26 23:20:44 +0800

    未完结更新

---
+++ RRQMSkin/MVVM/UIEngine/UIEngine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RRQMSkin.MVVM
+{
+    public class UIEngine<UI> where UI : UIElement
+    {
+        public UIEngine()
+        {
+            this.uis = new ConcurrentDictionary<object, UI>();
+        }
+        private readonly ConcurrentDictionary<object, UI> uis;
+        public UI RegisterUI<TUI>(object[] args) where TUI : UI
+        {
+            TUI obj = (TUI)Activator.CreateInstance(typeof(TUI), args);
+            this.uis.add
+        }
+
+        public UI GetUI<TUI>() where TUI : UI
+        {
+
+        }
+    }
+}
