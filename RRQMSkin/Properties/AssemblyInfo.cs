6e5b021d85125848a994a6f3a7bf13051f8c9e67
若汝棋茗 <1751615839@qq.com>
2022-03-21 21:50:57 +0800

    更新版本

---
+++ RRQMSkin/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: XmlnsDefinition("RRQM_MVVM", "RRQMSkin.MVVM")]
+[assembly: XmlnsDefinition("RRQM_Windows", "RRQMSkin.Windows")]
+[assembly: XmlnsDefinition("RRQM_Controls", "RRQMSkin.Controls")]
+[assembly: XmlnsDefinition("RRQM_DragDrop", "RRQMSkin.DragDrop")]
+[assembly: XmlnsDefinition("RRQM_Charts", "RRQMSkin.Charts")]
+[assembly: XmlnsDefinition("RRQM_Primitives", "RRQMSkin.Primitives")]
+
+// 在此类的 SDK 样式项目中，现在，在此文件中早前定义的几个程序集属性将在生成期间自动添加，并使用在项目属性中定义的值进行填充。有关包含的属性以及如何定制此过程的详细信息，请参阅
+// https://aka.ms/assembly-info-properties
+
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，请将该类型的 ComVisible
+// 属性设置为 true。
+
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID。
+
+[assembly: Guid("392318d1-30d4-4b3e-abfc-3e3d759b5413")]
--- RRQMSkinDemo/Properties/AssemblyInfo.cs
+++ RRQMSkinDemo/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -10,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RRQMSkinDemo")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
