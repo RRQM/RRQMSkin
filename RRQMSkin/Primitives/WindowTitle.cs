e63205cdce6acf0dd5a172fdc51ecf42fd6c9201
若汝棋茗 <505554090@qq.com>
2022-10-31 15:02:44 +0800

    T

---
+++ RRQMSkin/Primitives/WindowTitle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace RRQMSkin.Primitives
+{
+    public class WindowTitle : UIElement
+    {
+        private Window window;
+
+        public WindowTitle()
+        {
+            if (DesignerProperties.GetIsInDesignMode(this))
+            {
+                return;
+            }
+            this.window =Window.GetWindow(this);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (e.LeftButton== MouseButtonState.Pressed)
+            {
+                if (window != null)
+                {
+                    window.DragMove();
+                }
+            }
+           
+            base.OnMouseMove(e);
+        }
+    }
+}
